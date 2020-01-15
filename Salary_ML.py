9163360b8420788dfcbd40209cd80c3f785cdb95
zms3342 <zms3342@gmail.com>
2020-01-15 02:14:54 -0500

    printed name

--- Salary_ML.py
+++ Salary_ML.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 import seaborn as sns
 
+print(zafar)
 def clean_data(df):
     '''
     INPUT
